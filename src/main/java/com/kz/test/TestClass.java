2336e228fa41db93bbd0bd5fb2787c6e3ec97231
tbaizhumanov <tbaizhumanov@btes.com.kz>
2018-10-30 12:48:10 +0600

    :sparkles: add show method

--- src/main/java/com/kz/test/TestClass.java
+++ src/main/java/com/kz/test/TestClass.java
@@ -2,6 +2,13 @@ package com.kz.test;
 
 public class TestClass {
     public static void main(String[] args) {
-
+        TestClass testClass = new TestClass();
+        String a  = testClass.show("a");
     }
+
+    public String show(String val) {
+        return val;
+    }
+
+
 }
