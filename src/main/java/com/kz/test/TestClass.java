3452ce5ba2cbad7571034d5298a9c998e4efc14a
tbaizhumanov <tbaizhumanov@btes.com.kz>
2018-10-30 12:53:56 +0600

    :bulb: add summary

--- src/main/java/com/kz/test/TestClass.java
+++ src/main/java/com/kz/test/TestClass.java
@@ -6,9 +6,12 @@ public class TestClass {
         String a  = testClass.show("a");
     }
 
+    /**
+     * This method return new change string by template 'this -[%val%]'
+     * @param val input string
+     * @return 'this -[%val%]'
+     */
     public String show(String val) {
-        return val;
+        return "this -[" + val + "]";
     }
-
-
 }
